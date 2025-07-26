29f49985da6da4f04793e729dc8775a9abec97a6
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-07-26 16:06:08 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,6 +14,8 @@ from django.shortcuts import render,
 from django.http import HttpResponse
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def list_books(request):
