3df7ffd2634e5bb2941a2fb5440b14c8c2df3180
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-07-26 16:11:31 +0100

    update

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -41,4 +41,21 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
     
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+    
     
