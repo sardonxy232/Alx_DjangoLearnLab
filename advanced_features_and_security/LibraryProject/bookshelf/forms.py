16d8e8016490372cf06e02082b1a63535427cbea
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-07-28 00:03:42 +0100

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,9 +1,8 @@
 # bookshelf/forms.py
 
 from django import forms
-from .models import Book
+from .models import Book, CustomUser
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -17,3 +16,8 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2']
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
