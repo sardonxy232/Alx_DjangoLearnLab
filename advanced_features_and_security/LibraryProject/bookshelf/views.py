92d9a3f3afc5178404505dbb5fa23f830e60ff9d
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-07-28 00:27:37 +0100

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .forms import ExampleForm
+
 
 # Use Django ORM filtering to prevent SQL injection
 
